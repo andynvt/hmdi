f63d5a976eab0fd8671bf24a0c1c10349881a53d
chinguyen <chinguyen@lecle.co.kr>
2020-09-23 13:58:30 +0700

    Updated vehical_model_filter and vehicle_trim_filter

--- lib/module/vehicle_model_filter/vehicle_model_filter_view.dart
+++ lib/module/vehicle_model_filter/vehicle_model_filter_view.dart
@@ -16,8 +16,16 @@ class _VehicleModelFilterView extends StatefulWidget {
 }
 
 class _VehicleModelFilterViewState extends State<_VehicleModelFilterView> {
-  void _filterApplyClick() {
+  final List<bool> selected = [true, true, true, true, true, true, true, true, true, true];
+  void filterApplyClick() {
     Navigator.of(context).pop();
+  }
+
+  void resetClick() {
+    //TODO: reset filter
+  }
+  void _filterApplyClick() {
+    //TODO: apply filter
   }
 
   @override
@@ -25,20 +33,130 @@ class _VehicleModelFilterViewState extends State<_VehicleModelFilterView> {
     final model = Provider.of<VehicleModelFilterModel>(context);
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         automaticallyImplyLeading: false,
-        title: Text('Filter'),
+        brightness: Brightness.light,
+        elevation: 0,
+        title: Text(
+          'Filter',
+          style: TextStyle(color: Colors.black),
+        ),
         actions: [
           IconButton(
-            icon: Icon(Icons.close),
+            icon: Icon(
+              Icons.close,
+              color: Colors.black,
+            ),
             onPressed: Navigator.of(context).pop,
           )
         ],
       ),
-      body: Center(
-        child: FlatButton(
-          child: Text('Filter Apply'),
-          color: Colors.blue,
-          onPressed: _filterApplyClick,
+      body: Column(
+        children: [
+          Container(
+            height: 1,
+            color: Colors.grey[400],
+          ),
+          Container(
+            padding: const EdgeInsets.only(top: 10, right: 10),
+            alignment: Alignment.topRight,
+            child: FlatButton(
+              onPressed: resetClick,
+              child: Text(
+                'Reset',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.only(left: 15, bottom: 20),
+            child: buildItemFilter(8, 'Select All'),
+          ),
+          Container(
+            height: 1,
+            color: Colors.grey[400],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 15, right: 5),
+            child: Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: [
+                buildItemFilter(0, 'At a Glance'),
+                buildItemFilter(1, 'Exteior'),
+                buildItemFilter(2, 'Inteior'),
+                buildItemFilter(3, 'Primary Safety'),
+                buildItemFilter(4, 'Secondary Safety'),
+                buildItemFilter(5, 'Engine & Transmossion'),
+                buildItemFilter(6, 'Dimensions & Weights'),
+                buildItemFilter(7, 'Equipment'),
+              ],
+            ),
+          ),
+          Spacer(),
+          SafeArea(
+            child: Container(
+              width: double.infinity,
+              height: 60,
+              color: Colors.blue[900],
+              child: FlatButton(
+                onPressed: _filterApplyClick,
+                child: Text(
+                  'Filter Apply',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildItemFilter(int index, String title, {Function onchanged}) {
+    bool isSelect = true;
+    if (index < 8) {
+      isSelect = selected[index];
+    } else {
+      isSelect = !selected.contains(false);
+    }
+    return SizedBox(
+      height: 50,
+      child: FlatButton(
+        onPressed: () {
+          if (index == 8) {
+            setState(() {
+              selected.clear();
+              selected.addAll(List.generate(9, (index) => true));
+            });
+            return;
+          }
+          setState(() {
+            selected[index] = !selected[index];
+          });
+        },
+        color: isSelect ? Colors.lightBlue : Colors.grey[300],
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 12,
+            right: 12,
+          ),
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ),
       ),
     );
--- lib/module/vehicle_trim_filter/vehicle_trim_filter_view.dart
+++ lib/module/vehicle_trim_filter/vehicle_trim_filter_view.dart
@@ -24,21 +24,165 @@ class _VehicleTrimFilterViewState extends State<_VehicleTrimFilterView> {
   Widget build(BuildContext context) {
     final model = Provider.of<VehicleTrimFilterModel>(context);
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         automaticallyImplyLeading: false,
-        title: Text('Filter'),
+        brightness: Brightness.light,
+        elevation: 0,
+        title: Text(
+          'Filter',
+          style: TextStyle(color: Colors.black),
+        ),
         actions: [
           IconButton(
-            icon: Icon(Icons.close),
+            icon: Icon(
+              Icons.close,
+              color: Colors.black,
+            ),
             onPressed: Navigator.of(context).pop,
           )
         ],
       ),
-      body: Center(
-        child: FlatButton(
-          child: Text('Filter Apply'),
-          color: Colors.blue,
-          onPressed: _filterApplyClick,
+      body: Column(
+        children: [
+          Container(
+            height: 1,
+            color: Colors.grey[400],
+          ),
+          Expanded(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Text(
+                          'Required Field',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.blueAccent,
+                          size: 10,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Text(
+                          'Year',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.blueAccent,
+                          size: 10,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        buildYearSelect('2019'),
+                        SizedBox(width: 10),
+                        buildYearSelect('2020'),
+                        SizedBox(width: 10),
+                        buildYearSelect('2021'),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Text(
+                          'Model',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.blueAccent,
+                          size: 10,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: FlatButton.icon(
+                        onPressed: () {},
+                        label: Text(
+                          'KONA',
+                        ),
+                        icon: Icon(Icons.arrow_drop_down),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Exterior',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    Expanded(
+                      child: GridView.builder(
+                        itemCount: 7,
+                        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                          maxCrossAxisExtent: 200,
+                          crossAxisSpacing: 30,
+                          mainAxisSpacing: 30,
+                        ),
+                        padding: const EdgeInsets.only(right: 30, left: 30, top: 30, bottom: 30),
+                        itemBuilder: (_, index) {
+                          return Container(
+                            color: Colors.redAccent,
+                            child: Text('Red'),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Container(
+              width: double.infinity,
+              height: 60,
+              color: Colors.blue[900],
+              child: FlatButton(
+                onPressed: _filterApplyClick,
+                child: Text(
+                  'Filter Apply',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildYearSelect(String year) {
+    return Container(
+      height: 45,
+      child: FlatButton(
+        color: Colors.lightBlue,
+        onPressed: () {},
+        child: Text(
+          year,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
         ),
       ),
     );
